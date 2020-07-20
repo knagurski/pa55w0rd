59b992d3dffed22699eba01cf78107207bc98aa3
Kevin Nagurski <knagurski@gmail.com>
2020-07-21 00:13:10 +0100

    Initial Angular setup

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import Vue from "vue";
-import App from "./components/App.vue";
-
-new Vue({ render: createElement => createElement(App) }).$mount(".pa55w0rd");
--- src/state/password.js
+++
@@ -1,8 +0,0 @@
-import { generatePassword } from "../core/generator";
-
-export const password = {
-  password: ""
-};
-
-export const generate = config =>
-  (password.password = generatePassword(config));
